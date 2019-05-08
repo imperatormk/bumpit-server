bd44163753eb4830ffe1c223d9135b00d8b14566
Darko <darko.simonovski@bestisoft.com>
2019-05-08 18:58:49 +0200

    Fixed db config file Exporting db controllers from an index file

--- db/config/config.js
+++ db/config/config.js
@@ -2,27 +2,11 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const dbConfig = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-  }
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
 }
 
 module.exports = dbConfig
