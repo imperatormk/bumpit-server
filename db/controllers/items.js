353af7a52912e0f3c6c9f1366e90d60b465d9f5d
Darko <darko.simonovski@bestisoft.com>
2019-05-10 19:24:41 +0200

    Added modify item db function

--- db/controllers/items.js
+++ db/controllers/items.js
@@ -7,15 +7,19 @@ exportsObj.getItems = () => {
 }
 
 exportsObj.getItem = (itemId) => {
-	return Item.findOne({ where: { id: itemId }})
+	return Item.findOne({ where: { id: itemId } })
 }
 
 exportsObj.insertItem = (item) => {
 	return Item.create(item)
 }
 
+exportsObj.modifyItem = (item) => {
+	return Item.update(item, { where: { id: item.id } })
+}
+
 exportsObj.deleteItem = (itemId) => {
-	return Item.destroy({ where: { id: itemId }})
+	return Item.destroy({ where: { id: itemId } })
 	  .then(() => ({ id: itemId }))
 }
 
