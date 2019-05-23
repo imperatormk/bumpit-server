2bf58c882cc77b77c564d12ac316cb4f95fe6e5b
Darko <darko.simonovski@bestisoft.com>
2019-05-24 00:37:14 +0200

    Upgraded orders db controller

--- db/controllers/orders.js
+++ db/controllers/orders.js
@@ -1,17 +1,29 @@
 const exportsObj = {}
 
 const Order = require('../models').order
+const Item = require('../models').item
 
 exportsObj.getOrders = () => {
 	return Order.findAll()
 }
 
 exportsObj.getOrder = (ordId) => {
-	return Order.findOne({ where: { id: ordId }})
+	const options = {
+		include: [{
+			model: Item,
+			as: 'item'
+		}],
+		where: { id: ordId }
+	}
+	return Order.findOne(options)
 }
 
 exportsObj.insertOrder = (order) => {
 	return Order.create(order)
+}
+
+exportsObj.modifyOrder = (order) => {
+	return Order.update(order, { where: { id: order.id } })
 }
 
 exportsObj.deleteOrder = (ordId) => {
