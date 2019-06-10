cf3fd0711d7b35ee569c33c3ba27e4bedaa2a664
Darko <darko.simonovski@bestisoft.com>
2019-06-11 01:39:54 +0200

    Added endpoint for user shipping info

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -1,10 +1,19 @@
 const router = require('express').Router()
 
+const db = require(__basedir + '/db/controllers')
 const authMiddleware = require(__basedir + '/services/auth').middleware
 const login = require(__basedir + '/services/auth').login
 
 router.get('/user', authMiddleware, (req, res) => {
   return res.send(req.user)
+})
+
+router.get('/user/shippingInfo', authMiddleware, (req, res) => { // TODO: move this
+  const userId = req.user.id
+  return db.shippingInfos.getShippingInfoForUser(userId)
+    .then((shippingInfos) => {
+      res.send(shippingInfos)
+    })
 })
 
 router.post('/login', login)
