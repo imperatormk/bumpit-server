81139d61672ca326f79471f177da90e79c124fb2
Darko <darko.simonovski@bestisoft.com>
2019-06-19 21:16:44 +0200

    Cleaned up storage and upload utils

--- helpers/index.js
+++ helpers/index.js
@@ -1,51 +1,34 @@
 const exportsObj = {}
+
 const path = require('path')
 const constants = require('../config/constants')
+
+const multer  = require('multer')
 const crypto = require('crypto')
 const mime = require('mime')
 
-const generateFilename = (origFilename) => {
-  const filename = getRandomStrings(10)
-  const extension = path.extname(origFilename)
-  return `${filename}${extension}`
-}
-exportsObj.generateFilename = generateFilename
-
-exportsObj.saveFile = (file, dir) => {
-  const filename = generateFilename(file.name)
-  return new Promise((resolve, reject) => {
-  	return file.mv(path.join(dir, filename), (err) => {
-      if (err) return reject(err)
-      return resolve({ filename })
-	  })
-  })
-}
-
-/* ---------------------------------------------- */
-
-const multer  = require('multer')
+// upload fns
 
 const upload = (subFolder) => {
   if (!subFolder) throw { status: 'subfolder_missing' }
 
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      const storagePath = path.join(process.env.APP_STORAGE_PATH, subFolder)
+      const storagePath = getStoragePath(subFolder)
       cb(null, storagePath)
     },
     filename: (req, file, cb) => {
       crypto.pseudoRandomBytes(16, (err, raw) => {
-        cb(null, raw.toString('hex') + Date.now() + '.' + mime.getExtension(file.mimetype))
+        cb(null, `${raw.toString('hex')}-${Date.now()}.${mime.getExtension(file.mimetype)}`)
       })
     }
   })
 
   return multer({ storage: storage })
 }
-
 exportsObj.uploadMiddleware = upload
 
-/* ---------------------------------------------- */
+// storage utils
 
 const getStoragePath = (key) => {
   const baseStoragePath = constants.appStoragePath
@@ -58,6 +41,8 @@ const getStorageUrl = (key, filename) => {
   return path.join(storagePath, filename)
 }
 exportsObj.getStorageUrl = getStorageUrl
+
+// static files utils
 
 const getStaticFilesPath = (key) => {
   const baseStaticFilesPath = constants.staticFilesPath
